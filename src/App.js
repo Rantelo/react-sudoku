cceff48f13653de349b1cd9c3a1e1641fe8da3c2
Roberto Antelo <roberto_antelo@hotmail.com>
2016-09-12 08:13:56 -0500

    First approach of a method to render the board

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 export default class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this._generateRow = this._generateRow.bind(this);
+  }
+  _generateRow(arr, row) {
     return (
       <div>
+        {arr.map((element,index) => {
+          return(
+            <div key={`${row}-${index}`} className={'box'}> {element}</div>
+          );
+        })}
+      </div>
+    )
+  }
+  render() {
+    let sudoku = [
+      [1,2,3,4,5,6,7,8,9],
+      [4,5,6,7,8,9,1,2,3],
+      [7,8,9,1,2,3,4,5,6]
+    ];
+    let rows = sudoku.map((row, index) => this._generateRow(row,index));
+
+    return (
+      <div>
+        {rows}
         Shudokuu
       </div>
     );

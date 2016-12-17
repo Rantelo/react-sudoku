da0551798fd237a07ed9bb6310ce31028ce6f1e6
Roberto Antelo <roberto_antelo@hotmail.com>
2016-12-17 06:50:09 -0400

    Complete row, column and block validation

--- src/helpers.js
+++ src/helpers.js
@@ -1,25 +1,32 @@
-export const isOkToInsert = ( coords, matrix) => {
+export const isOkToInsert = ( number, coords, matrix) => {
   const [ x, y ] = coords;
-  console.log('column', getColumn(y, matrix));
-  console.log('row', getRow(x, matrix));
-  return true;
+  const col      = getColumn(y, matrix);
+  const row      = getRow(x, matrix);
+  const block    = getBlock(coords, matrix);
+
+  return isNotInSet(number, col) && isNotInSet(number, row) && isNotInSet(number, block);
 }
 
-const isNotInSet = ( number, set ) => {
-  return true;
+const isNotInSet = ( number, set) => !set.has(number)
 
-}
+/* returns a set */
+const getColumn = ( col, matrix ) => new Set(matrix.map(row => row[col]));
 
-const getColumn = ( col, matrix ) => matrix.map(row => row[col]);
+/* returns a set */
+const getRow = ( row, matrix ) => new Set(matrix[row]);
 
-const getRow = ( row, matrix ) => matrix[row];
-
+/* returns a set */
 const getBlock = ( coords, matrix ) => {
   const [ x, y ] = coords;
+  const cuadrant = [parseInt(x/3), parseInt(y/3)];
+  const start    = [cuadrant[0]*3, cuadrant[1]*3];
+  let   ans      = new Set();
 
+  for ( let i = start[0]; i < start[0]+3; i++ ) {
+    for ( let j = start[1]; j < start[1]+3; j++ ) {
+      ans.add(matrix[i][j]);
+    }
+  }
 
-
-
-  return column
-
+  return ans;
 }
